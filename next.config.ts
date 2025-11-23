03b57d92ddf437d68fc87fc64071c3ebcec14f62
곽세령 <candice@syncbridge.co.kr>
2025-11-23 14:43:46 +0900

    feat: 배포를 위한 nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/ksr_portfolio_251123",
+  assetPrefix: "/ksr_portfolio_251123/",
 };
 
 export default nextConfig;
