ffdc422bab1bc19e066e1772ebe86e3af7bed78c
곽세령 <candice@syncbridge.co.kr>
2025-11-23 14:58:34 +0900

    Remove GitHub Pages config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/ksr_portfolio_251123",
-  assetPrefix: "/ksr_portfolio_251123/",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
